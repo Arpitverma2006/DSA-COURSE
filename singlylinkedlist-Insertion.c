12d2445822a9bda852638693d9e832c00474a113
Arpitverma2006 <vermaajay6799013@gmail.com>
2024-04-04 17:35:49 +0530

    Insertion Operation

--- singlylinkedlist-Insertion.c
+++ singlylinkedlist-Insertion.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+// Insertion at Beginning & start
+#include<stdio.h>  
 #include<stdlib.h>
 void beginsert(int);
 stuct node{
@@ -28,4 +29,229 @@ void beginsert(int item){
 		head=ptr;
 		printf("\n Node Inserted \n");
 	}
+}
+
+
+
+// Insertion at Specific Position 
+#include<stdio.h>  
+#include<stdlib.h>  
+void randominsert(int);  
+void create(int);  
+struct node  
+{  
+    int data;  
+    struct node *next;  
+};  
+struct node *head;  
+void main ()  
+{  
+    int choice,item,loc;  
+    do   
+    {  
+        printf("\nEnter the item which you want to insert?\n");  
+        scanf("%d",&item);  
+        if(head == NULL)  
+        {  
+            create(item);  
+        }  
+        else  
+        {  
+            randominsert(item);  
+        }  
+        printf("\nPress 0 to insert more ?\n");  
+        scanf("%d",&choice);  
+    }while(choice == 0);  
+}  
+void create(int item)  
+{  
+      
+        struct node *ptr = (struct node *)malloc(sizeof(struct node *));  
+        if(ptr == NULL)  
+        {  
+            printf("\nOVERFLOW\n");  
+        }  
+        else  
+        {  
+            ptr->data = item;  
+            ptr->next = head;  
+            head = ptr;  
+            printf("\nNode inserted\n");  
+        }  
+}  
+void randominsert(int item)  
+    {  
+        struct node *ptr = (struct node *) malloc (sizeof(struct node));  
+        struct node *temp;  
+        int i,loc;  
+        if(ptr == NULL)  
+        {  
+            printf("\nOVERFLOW");  
+        }  
+        else  
+        {  
+              
+            printf("Enter the location");  
+            scanf("%d",&loc);             
+            ptr->data = item;  
+            temp=head;  
+            for(i=0;i<loc;i++)  
+            {  
+                temp = temp->next;  
+                if(temp == NULL)  
+                {  
+                    printf("\ncan't insert\n");  
+                    return;  
+                }  
+              
+            }  
+            ptr ->next = temp ->next;   
+            temp ->next = ptr;   
+            printf("\nNode inserted");  
+        }  
+          
+    }
+
+
+
+// Insertion at End 
+
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int data;           
+    struct node *next;  
+}*head;
+
+
+void createList(int n);
+void insertNodeAtEnd(int data);
+void displayList();
+
+
+int main()
+{
+    int n, data;
+
+    /*
+     * Create a singly linked list of n nodes
+     */
+    printf("Enter the total number of nodes: ");
+    scanf("%d", &n);
+    createList(n);
+
+    printf("\nData in the list \n");
+    displayList();
+
+    /*
+     * Insert data at the end of the singly linked list
+     */
+    printf("\nEnter data to insert at end of the list: ");
+    scanf("%d", &data);
+    insertNodeAtEnd(data);
+
+    printf("\nData in the list \n");
+    displayList();
+
+    return 0;
+}
+
+void createList(int n)
+{
+    struct node *newNode, *temp;
+    int data, i;
+    head = (struct node *)malloc(sizeof(struct node));
+    if(head == NULL)
+    {
+        printf("Unable to allocate memory.");
+    }
+    else
+    {
+        printf("Enter the data of node 1: ");
+        scanf("%d", &data);
+
+        head->data = data; // Link the data field with data
+        head->next = NULL; // Link the address field to NULL
+
+        temp = head;
+        for(i=2; i<=n; i++)
+        {
+            newNode = (struct node *)malloc(sizeof(struct node));
+            if(newNode == NULL)
+            {
+                printf("Unable to allocate memory.");
+                break;
+            }
+            else
+            {
+                printf("Enter the data of node %d: ", i);
+                scanf("%d", &data);
+
+                newNode->data = data; // Link the data field of newNode with data
+                newNode->next = NULL; // Link the address field of newNode with NULL
+
+                temp->next = newNode; // Link previous node i.e. temp to the newNode
+                temp = temp->next; 
+            }
+        }
+
+        printf("SINGLY LINKED LIST CREATED SUCCESSFULLY\n");
+    }
+}
+
+
+/*
+ * Create a new node and inserts at the end of the linked list.
+ */
+void insertNodeAtEnd(int data)
+{
+    struct node *newNode, *temp;
+
+    newNode = (struct node*)malloc(sizeof(struct node));
+
+    if(newNode == NULL)
+    {
+        printf("Unable to allocate memory.");
+    }
+    else
+    {
+        newNode->data = data; // Link the data part
+        newNode->next = NULL; 
+
+        temp = head;
+
+        // Traverse to the last node
+        while(temp != NULL && temp->next != NULL)
+            temp = temp->next;
+
+        temp->next = newNode; // Link address part
+
+        printf("DATA INSERTED SUCCESSFULLY\n");
+    }
+}
+
+
+/*
+ * Display entire list
+ */
+void displayList()
+{
+    struct node *temp;
+
+    /*
+     * If the list is empty i.e. head = NULL
+     */
+    if(head == NULL)
+    {
+        printf("List is empty.");
+    }
+    else
+    {
+        temp = head;
+        while(temp != NULL)
+        {
+            printf("Data = %d\n", temp->data); // Print data of current node
+            temp = temp->next;                 // Move to next node
+        }
+    }
 }
